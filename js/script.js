2c698a5347c26473e568a1500bd18aeed88845f9
Melozzo <carmecamma1995@gmail.com>
2023-09-21 15:58:29 +0200

    [add]: cicle for...of  and console log

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-const team = [
+//1. array oggetti con info personale
+const team = 
+[
   {
-    name: 'Wayne Barnett',
+    name: 'Wayne',
     role: 'Founder & CEO',
     image: 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+  name: 'Gaspare',
+  role: 'Founder & CEO',
+  image: 'wayne-barnett-founder-ceo.jpg'
   }
 ]
-console.log(team);
+
+//2. visualizzazione info
+for(let person of team){
+  console.log(person.image);
+  console.log(person.name);
+  console.log(person.role);
+  console.log('==================')
+}
